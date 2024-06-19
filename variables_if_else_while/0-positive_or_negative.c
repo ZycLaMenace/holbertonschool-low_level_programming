8328a05d1a384bbbb546e0b13f75478aee4c2b00
zyclamenace <gemicieneshakki@gmail.com>
2024-06-19 16:06:54 +0200

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 
-/* main - Nombre random qui renvoie négative ou positif ou = 0 
+/**  main - Nombre random qui renvoie négative ou positif ou = 0
 *  Return: + ou - ou 0
 */
 int main(void)
@@ -13,18 +13,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n < 0)
     {
-        printf("%d is negative\n", n);
-    
+    printf("%d is negative\n", n);
     }
     else if (n == 0)
     {
-        printf("%d is zero\n",n);
+    printf("%d is zero\n", n);
     }
     else 
     {
-        printf("%d is positive\n", n);
+    printf("%d is positive\n", n);
     }
-    
-    
 	return (0);
 }

7b05b91a23f31c29975e81bf3e96e58df25c3ed1
zyclamenace <gemicieneshakki@gmail.com>
2024-06-20 14:43:33 +0200

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Desc de la fonction
+ * Return: Desc de la fonction
+*/
+
+int main(void)
+{
+char num;
+for (num == 0; num < 10; num++)
+{
+	putchar(num);
+}
+putchar('\n');
+	return (0);
+}

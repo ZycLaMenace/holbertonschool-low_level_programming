a92bde389757cf4b18aea40398bd9522410db421
zyclamenace <gemicieneshakki@gmail.com>
2024-06-20 14:33:07 +0200

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Desc de la fonction
+ * Return: Desc de la fonction
+*/
+
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter != 'e' && letter != 'q' && letter <= 'z'; letter++)
+{
+	putchar(letter);
+}
+putchar('\n');
+	return (0);
+}

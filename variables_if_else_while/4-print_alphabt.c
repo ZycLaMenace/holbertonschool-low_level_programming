8b76ba4683dc3f4bb33f813cb0275fe3b1a3bc06
zyclamenace <gemicieneshakki@gmail.com>
2024-06-20 14:51:21 +0200

    4-print_alphabt.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 char letter;
-for (letter = 'a'; letter != 'e', letter != 'q', letter <= 'z'; letter++)
+for (letter = 'a'; letter <= 'z'; letter != 'e' && letter != 'q' && letter++)
 {
 	putchar(letter);
 }

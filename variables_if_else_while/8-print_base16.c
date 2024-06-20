3ac88839fbd98ded374717c8a5f62baf6721886f
zyclamenace <gemicieneshakki@gmail.com>
2024-06-20 15:50:06 +0200

    8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Desc de la fonction
+ * Return: Desc de la fonction
+*/
+
+int main(void)
+{
+char letter;
+char num;
+
+for (num = 0; num < 9; num++)
+{
+	putchar(num);
+}
+for (letter = 'a'; letter <= 'f'; letter++)
+{
+	putchar(letter);
+}
+putchar('\n');
+	return (0);
+}

3f407a1a83c4a72c277f791826e1e2e2fc5f0c12
zyclamenace <gemicieneshakki@gmail.com>
2024-06-20 13:54:47 +0200

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Desc de la fonction
+ * Return: Desc de la fonction
+*/
+int main(void)
+{
+
+	for (int i = 0; i < 26; i++)
+	{
+	putchar('a');
+	}
+	return (0);
+}

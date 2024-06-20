ab0c230f14619f315baecafcabf38e0529f606c3
zyclamenace <gemicieneshakki@gmail.com>
2024-06-20 15:18:52 +0200

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Desc de la fonction
+ * Return: Desc de la fonction
+*/
+
+int main(void)
+{
+char letter;
+for (letter = 'z'; letter <= 'a'; letter--)
+{
+	putchar(letter);
+}
+putchar('\n');
+	return (0);
+}

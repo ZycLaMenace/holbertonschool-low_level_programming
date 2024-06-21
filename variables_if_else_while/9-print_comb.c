54278f524f8c8be006139e5df52bafb41fd6aa5e
zyclamenace <gemicieneshakki@gmail.com>
2024-06-21 09:42:52 +0200

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Desc de la fonction
+ * Return: Desc de la fonction
+*/
+
+int main(void)
+{
+char num;
+for (num = '0'; num <= '9'; num++)
+{
+	putchar(num);
+	putchar(',');
+	putchar(' ');
+}
+putchar('\n');
+	return (0);
+}

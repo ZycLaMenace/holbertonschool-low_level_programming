841378ce496d9636dc362d21347a698eeaee85fc
zyclamenace <gemicieneshakki@gmail.com>
2024-06-21 11:34:32 +0200

    0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Ce que fais main
+ * Return: 0 pour success
+*/
+
+int main(void){
+	//programme pour crée _putchar
+	int pgrm;
+	printf("_putchar\n");
+	return (0);
+}

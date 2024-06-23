fc26ab987f9f521ab73ec2438112aa16dccd1b7e
zyclamenace <gemicieneshakki@gmail.com>
2024-06-23 16:49:50 +0200

    4-isalpha.c

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - savoir si c'est une lettre
+ * @c: la variable
+ * Return: Always 0 or 1 for lowercase
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' && c >= 'A' && c <= 'Z')
+	return (1);
+	else
+	return (0);
+}

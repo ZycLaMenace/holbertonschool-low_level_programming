1877d84d92f8a53bab029440474eec4c6c95e095
zyclamenace <gemicieneshakki@gmail.com>
2024-06-23 16:17:48 +0200

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -8,8 +8,9 @@
 void print_alphabet_x10(void)
 {
 int loop10;
-for (loop10 = 0; loop10 < 10; loop10++) {
-		int i;
+for (loop10 = 0; loop10 < 10; loop10++) 
+{
+	int i;
 	for (i = 'a'; i <= 'z'; i++)
 {
 	_putchar(i);

ee66cb5b6cd2c06d902a66ef018abc90398149b9
zyclamenace <gemicieneshakki@gmail.com>
2024-06-23 15:44:23 +0200

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,16 +1,20 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * main - Ce que fais main
  * Return: 0 pour success
 */
- char c[] = '_putchar';
- int i ;
-int main(void) {
-	for(i = 0; i != 0; i++){
-
-	_putchar(c[i]);
-	}
+ 
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-	return (0);
+
+return (0);
 }

27ccdc642b377ca719e255625c0f4bdb6e305cd4
zyclamenace <gemicieneshakki@gmail.com>
2024-06-23 16:16:26 +0200

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Desc de la fonction
+ * Return: return rs
+*/
+
+void print_alphabet_x10(void)
+{
+int loop10;
+for(loop10 = 0; loop10 < 10; loop10++) {
+		int i;
+	for (i = 'a'; i <= 'z'; i++)
+{
+	_putchar(i);
+}
+_putchar('\n');
+}
+}

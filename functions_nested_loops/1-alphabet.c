9ab7f16f01aef69655bae7e6b641e1c9c4f09343
zyclamenace <gemicieneshakki@gmail.com>
2024-06-23 15:57:20 +0200

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Desc de la fonction
+ * Return: return rs
+*/
+
+void print_alphabet(void);
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+	_putchar(letter);
+}
+	_putchar('\n');
+	return;
+}

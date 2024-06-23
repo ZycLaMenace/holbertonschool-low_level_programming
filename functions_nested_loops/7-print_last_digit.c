fadf412634d6a23988f4edd1f19f015a130e867d
zyclamenace <gemicieneshakki@gmail.com>
2024-06-23 17:41:48 +0200

    7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -2,14 +2,14 @@
 
 /**
  * _int print_last_digit(int) - check the code
- * @a: variable
+ * @ls: variable
  * Return: Always 0.
  */
 
 int print_last_digit(int ls)
 {
-    ls = ls % 10;
-    if (ls < 0)
-    ls = -ls;    
-    return(ls);
+	ls = ls % 10;
+	if (ls < 0)
+	ls = -ls;    
+	return(ls);
 }

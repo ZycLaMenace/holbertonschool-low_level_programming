b9827c859a178ae6ccbc0e60fa298f37b0beb403
zyclamenace <gemicieneshakki@gmail.com>
2024-06-23 16:46:34 +0200

    3-islower.c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
  * islower - savoir si c'est miniscule
- * @c: la variable 
+ * @c: la variable
  * Return: Always 0 or 1 for lowercase
  */
 
- int _islower(int c)
+int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 	return (1);

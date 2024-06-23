5db6e96232790d0f61d874500aaf5c2235ca0b4a
zyclamenace <gemicieneshakki@gmail.com>
2024-06-23 17:41:37 +0200

    7-print_last_digit.c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _int print_last_digit(int) - check the code
+ * @a: variable
+ * Return: Always 0.
+ */
+
+int print_last_digit(int ls)
+{
+    ls = ls % 10;
+    if (ls < 0)
+    ls = -ls;    
+    return(ls);
+}

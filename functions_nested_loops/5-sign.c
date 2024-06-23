5c282a6eebd5d0e4b48ed271df96a1f939db4975
zyclamenace <gemicieneshakki@gmail.com>
2024-06-23 17:15:33 +0200

    5-sign.c

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ * @c: varible
+ * Return: Always 0 or return zero or return 1
+ */
+
+int print_sign(int n)
+{
+int number;
+	if (number = 0)
+	{
+		return (0);
+	}
+	if (number < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('+');
+		return (1);
+	}
+}

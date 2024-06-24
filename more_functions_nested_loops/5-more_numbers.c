4d573257fb3e0284aade1fdf7841b94ea048e84c
zyclamenace <gemicieneshakki@gmail.com>
2024-06-24 14:07:49 +0200

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -12,14 +12,14 @@ void more_numbers(void)
 	int loop10;
 	for (loop10 = 0; loop10 < 10; loop10++)
 		{
-			int i;
+			int i, a;
 			for (i = '0'; i <= '9'; i++)
 			{
 				_putchar(i);
 			}
-			for (i = '0'; i <= '4'; i++)
+			for (a = '0'; a <= '4'; a++)
 			{
-				_putchar('1' + i);
+				_putchar('1' + a);
 			}
 	_putchar('\n');
 	}

e5093db0de073d57840deb486878675f51cd159b
zyclamenace <gemicieneshakki@gmail.com>
2024-06-24 11:55:50 +0200

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplie deux variables
+ * @a: variable a
+ * @b: variable b
+ * Return: le resultat
+ */
+
+int mul(int a, int b)
+{
+	int n = a * b;
+
+	return (n);
+}

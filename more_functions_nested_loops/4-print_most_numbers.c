d0e8dcf7f7fe533123e6a7539c53358430269d08
zyclamenace <gemicieneshakki@gmail.com>
2024-06-24 12:37:25 +0200

    4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_numbers - ecris les nombres
+ * @i; variable
+ *
+ */
+
+void print_most_numbers(void)
+
+{
+	int i;
+		for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+        if (i = '2')
+        continue;
+		if (i = '4')
+		continue;
+	}
+	_putchar('\n');
+}

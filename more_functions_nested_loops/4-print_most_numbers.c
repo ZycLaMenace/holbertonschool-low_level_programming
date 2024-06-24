14de7a8123e4f9390c6803db968bcd4dfd315be9
zyclamenace <gemicieneshakki@gmail.com>
2024-06-24 12:44:24 +0200

    4-print_most_numbers.c

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -12,10 +12,7 @@ void print_most_numbers(void)
 	int i;
 		for (i = '0'; i <= '9'; i++)
 	{
-        if (i = 2)
-        	continue;
-		if (i = 4)
-			continue;
+        if (i == '2' || i == '4')
 		_putchar(i);
 	}
 	_putchar('\n');

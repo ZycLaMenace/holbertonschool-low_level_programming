71e58d85eb3f50dbeefe801d8b5223ddb897f950
zyclamenace <gemicieneshakki@gmail.com>
2024-06-24 12:01:36 +0200

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - ecris les nombres
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void);
+
+{
+	for (print_numbers = 0; print_numbers <=9; print_numbers++)
+	{
+		_putchar('print_numbers');
+	}
+	_putchar('\n');
+}

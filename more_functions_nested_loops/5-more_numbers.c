0a5deb38c2632495c404b1d29e41bb3e9dc3ee2b
zyclamenace <gemicieneshakki@gmail.com>
2024-06-24 13:13:29 +0200

    5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - ecris les nombres
+ * @i; variable
+ * Return; les nombres sans 2 et 4
+ */
+
+void more_numbers(void);
+
+{
+	int i;
+		for (i = '0'; i <= '14'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}

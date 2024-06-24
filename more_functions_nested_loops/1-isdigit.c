489dc3ad69c4f7de8bb376fca8d385359773b1e1
zyclamenace <gemicieneshakki@gmail.com>
2024-06-24 11:54:06 +0200

    1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - isUpper savoir si c'est une maj.
+ * @c: variable
+ * Return: Always 0 pour une miniscule. et 1 pour MAJ
+ */
+
+int _isdigit(int c)
+
+{
+	if (c >= '0' && c <= '9')
+	    return (1);
+	else
+	    return (0);
+}

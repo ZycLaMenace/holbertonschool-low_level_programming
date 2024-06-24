9131bc24df4700ad6507140baa15c37c746d4c01
zyclamenace <gemicieneshakki@gmail.com>
2024-06-24 15:26:36 +0200

    6-print_line.c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line - trace une ligne
+ * @n; longuer de la ligne
+ * Return; une ligne
+ */
+
+ void print_line(int n)
+ {
+	int linelen;
+
+	for (linelen = 0; linelen <= n; linelen++)
+	{
+	if (n <= 0)
+		{
+			_putchar('\n');
+		}
+	else
+	{
+		_putchar('_');
+		
+	}
+	}
+	_putchar('\n');
+ }
+ 

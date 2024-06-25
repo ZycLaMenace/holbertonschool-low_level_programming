cfaa318062099231846b71cec7fbb3e283b71700
zyclamenace <gemicieneshakki@gmail.com>
2024-06-25 09:55:55 +0200

    8-print_square.c

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,27 +1,27 @@
 #include "main.h"
 
 /**
- * print_diagonal - fais une diagonale
- *@n: variable
+ * print_square - fais un carre
+ *@size: variable
  * Return: Always 0.
  */
 
 void print_square(int size)
 
 {
-    int side;
-    int carre;
+	int side;
+	int carre;
 
-    if (size <= 0)
+	if (size <= 0)
 	{
 		_putchar('\n');
 	}
-    for (carre = 0; carre < size ; carre++)
-    {
-        for (side = 0; side < size; side++)
-            {
-		    _putchar('#');
-        }
-    _putchar('\n');
-    }
+	for (carre = 0; carre < size ; carre++)
+	{
+		for (side = 0; side < size; side++)
+			{
+			_putchar('#');
+		}
+	_putchar('\n');
+	}
 }

946f3666bda99aa9b8a7f62fd6f1c4e0078e934e
zyclamenace <gemicieneshakki@gmail.com>
2024-06-26 10:14:49 +0200

    10-print_triangle.c

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - fais une diagonale
+ *
+ * Return: Always le triangle
+ */
+
+void print_triangle(int n)
+
+{
+int espace;
+int hashtag;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+    {
+        for (espace = 0; espace <= n; espace++)
+        {
+        for (hashtag = 0; hashtag < n - espace; hashtag++)
+        {
+            _putchar(' ');
+        }
+        for (hashtag = 0; hashtag < espace; hashtag++)
+		{
+            _putchar('#');
+        }
+        _putchar('\n');
+    }
+}
+}

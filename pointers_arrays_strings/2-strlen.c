7a54462ff39e61cfa426a127d1a84cf715f3aad7
zyclamenace <gemicieneshakki@gmail.com>
2024-06-27 14:55:59 +0200

    task 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - swap les pointer
+ *@s: pointos 1
+ *@longueur: pointos 2
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+
+{
+	int longueur;
+
+	for (longueur = 0; s[longueur] != '\0'; longueur++);
+	return (longueur);
+}

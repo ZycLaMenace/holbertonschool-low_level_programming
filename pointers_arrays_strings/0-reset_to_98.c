1028ec2611a7f145d29eb3c8addbac7e8bea6705
zyclamenace <gemicieneshakki@gmail.com>
2024-06-27 13:11:12 +0200

    task 0 et 1

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,7 @@
 #include <stdio.h>
 
 /**
- * main - check the code 
- *
+ * reset_to_98 - change la variable en 98
  * Return: Always 0.
  */
 
---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - swap les pointer
+ *
+ * Return: Always 0.
+ */
+
+
+void swap_int(int *a, int *b)
+{
+    swap_int(*a = *b, *b = *a);
+}

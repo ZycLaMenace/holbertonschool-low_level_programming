ac90637a4aed1f769401be2f15b8263a220712fc
zyclamenace <gemicieneshakki@gmail.com>
2024-06-30 17:23:10 +0200

    4-print_rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - ecrire txt a l'envers
+ *@s: pointos
+ * Return: Always 0.
+ */
+
+void print_rev(char *s)
+{
+	int txt;
+
+	for (txt = 0; s[txt]; txt++)
+	{
+    }
+    txt--;
+    while (txt >= 0)
+    {
+        _putchar(s[txt]);
+        txt--;
+    }
+	_putchar('\n');
+}

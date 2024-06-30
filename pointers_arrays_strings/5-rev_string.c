5ff9f437e365d3adc2bf36e2f2c11a9039040c9a
zyclamenace <gemicieneshakki@gmail.com>
2024-06-30 18:29:07 +0200

    5-rev_string.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include "2-strlen.c"
+
+/**
+ * rev_string - ecrire qtt en reverse
+ - swap les pointer
+ *@s: pointos
+ * Return: Always 0.
+ */
+
+void rev_string(char *s)
+{
+char *start = s;
+char *end = s + _strlen(s) - 1;
+
+while (start < end)
+{
+    char tmp = *start;
+    *start = *end;
+    *end = tmp;
+    start++;
+    end--;
+}
+}

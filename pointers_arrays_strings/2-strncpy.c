25a7799f531898fb528b6b81269495f0498a58e0
zyclamenace <gemicieneshakki@gmail.com>
2024-07-02 10:41:37 +0200

    task 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "2-strlen.c"
+
+ /** _strncpy - concatenates two strings
+ *@dest: destiantion string
+ *@src: source string
+ * Return: Always 0.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+    int i;
+
+    for (i = 0; i < n && src[i] != '\0'; i++)
+    {
+        dest[i] = src[i];
+    }
+    for ( ; i < n; i++)
+    {
+        dest[i] = '\0';
+    }
+   return dest;
+}

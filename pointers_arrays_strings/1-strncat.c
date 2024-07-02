eca0b7aabe5de889298c7000d4c0881ab5373395
zyclamenace <gemicieneshakki@gmail.com>
2024-07-02 10:37:08 +0200

    task 1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,15 +1,15 @@
 #include "main.h"
 #include "2-strlen.c"
 
-/**
- * _strncat - check the code
- *
+ /** _strncat - concatenates two strings
+ *@dest: destiantion string
+ *@src: source
  * Return: Always 0.
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-    char dest_len = _strlen(dest);
+	char dest_len = _strlen(dest);
 	int i;
 
    for (i = 0 ; i < n && src[i] != '\0' ; i++)

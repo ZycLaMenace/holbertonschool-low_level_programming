4278fa9a0b340a9fa23c23f0d30a3f78f46ed8b2
zyclamenace <gemicieneshakki@gmail.com>
2024-07-02 14:25:22 +0200

    task 0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,15 +1,19 @@
 #include "main.h"
 
 /**
- * _memset - set memory
- *@s:
- * Return: memory area
+ * _memset - fills the first n bytes of the memory area
+ *      			pointed to by s with the constant byte b.
+ *@b: byte
+ *@n: number of byte
+ *@s: pointer s
+ * Return: memory area, a pointer to the memory area s
  */
 
-char *_memset(char *s, char b, unsigned int n) 
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-	for (i = 0; i < n; i++) 
+
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}

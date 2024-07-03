b91fb1bc4402e59f939db712e91b8dd5a8d0f3c1
zyclamenace <gemicieneshakki@gmail.com>
2024-07-03 15:55:32 +0200

    task 1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy - copy memory 
+ *@n: byte
+ *@dest: destination
+ *@src: source
+ * Return: memory area, a pointer to the memory area s
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	char tmp;
+
+	for (i = 0; i < n; i++)
+	{
+		tmp = src[i];
+		dest[i] = tmp;
+	}
+	return (dest);
+}

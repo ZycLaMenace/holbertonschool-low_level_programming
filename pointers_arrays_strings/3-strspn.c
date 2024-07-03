68323fe9a685c45ac8d3725975c69f5fb932841c
zyclamenace <gemicieneshakki@gmail.com>
2024-07-03 20:20:43 +0200

    task 3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strspn - search accepted char in string
+ * @s: string 
+ * @accept: accepted char
+ *
+ * Return: number of accepted char in string
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0;
+
+	while (*s)
+	{
+		int j = 0;
+
+		while (accept[j])
+		{
+			if (*s == accept[j])
+			{
+				j = -1;
+				break;
+			}
+			j++;
+		}
+
+		if (j != -1)
+		{
+			break;
+		}
+
+		s++;
+		i++;
+	}
+
+	return (i);
+}

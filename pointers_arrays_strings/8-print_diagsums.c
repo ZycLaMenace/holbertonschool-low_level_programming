3d9cfbf53e4ef029610ad8e1d9ec715379f106cb
zyclamenace <gemicieneshakki@gmail.com>
2024-07-05 09:43:55 +0200

    task 8

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - donne la somme d'une diagonale du matrice
+ * @a: matrice
+ * @size: taille matrice
+ * 
+*/
+
+void print_diagsums(int *a, int size)
+{
+	int sum1 = 0, sum2 = 0;
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[i * size + i];
+
+		sum2 += a[i * size + (size - 1 - i)];
+	}
+
+	printf("%d, %d\n", sum1, sum2);
+}

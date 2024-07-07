75bf2f15648ecf03e014e8bfa41fa1bdd534107e
zyclamenace <gemicieneshakki@gmail.com>
2024-07-08 00:00:45 +0200

    task 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - program
+ * @argc: number of arguments
+ * @argv: array of argc
+ *
+ * Return: EXIT_SUCESS
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+return (EXIT_SUCCESS);
+}

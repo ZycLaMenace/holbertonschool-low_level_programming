21e6d5444dce3ac4c8a6b0468f496cb96961ef35
zyclamenace <gemicieneshakki@gmail.com>
2024-07-09 11:36:05 +0200

    task 2 concatenate

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -14,28 +14,27 @@ char *str_concat(char *s1, char *s2)
 	char *concat;
 	unsigned int i, j, len1 = 0, len2 = 0;
 
-if (s1 == 0 )
+if (s1 == 0)
 		s1 = "";
-if (s2 == 0 )
+if (s2 == 0)
 		s2 = "";
 
-	 while (s1[len1])
+	while (s1[len1])
 		len1++;
 
 	while(s2[len2])
 		len2++;
 
 concat = malloc((len1 + len2 + 1) * sizeof(char));
-
-if (concat == NULL)
+	if (concat == NULL)
 		return (NULL);
 
-	for (i = 0; i < len1 ; i++)
+	for (i = 0; i < len1; i++)
 	{
 		concat[i] = s1[i];
 	}
 
-	for (j = 0; j < len2 ; j++ ,i++)
+	for (j = 0; j < len2; j++, i++)
 	{
 		concat[i] = s2[j];
 	}

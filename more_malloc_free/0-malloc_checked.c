83478c7e40586b14bdf4cb4f4479f303c50894ee
zyclamenace <gemicieneshakki@gmail.com>
2024-07-11 13:03:05 +0200

    task 0

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -4,16 +4,16 @@
 /**
  * malloc_checked - Check if malloc work
  * @b: bytes
- * 
+ *
  * Return: res or 98 for failrue
 */
 
 void *malloc_checked(unsigned int b)
 {
 	void *res;
-	
+
 	res = malloc(b);
 	if (res == NULL)
 		exit(98);
-	return(res);
+	return (res);
 }

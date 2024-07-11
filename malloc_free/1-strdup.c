f5594e57d8944216504b00a34a3d8b2084188d0e
zyclamenace <gemicieneshakki@gmail.com>
2024-07-11 11:12:39 +0200

    task 1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -17,13 +17,13 @@ char *_strdup(char *str)
 if (str == 0)
 	return (NULL);
 
-	 while (str[len])
+	while (str[len])
 	len++;
 
 	array = malloc((len + 1) * sizeof(char));
 
 if (array == NULL)
-		return (NULL);
+	return (NULL);
 
 	for (i = 0; i <= len ; i++)
 	{

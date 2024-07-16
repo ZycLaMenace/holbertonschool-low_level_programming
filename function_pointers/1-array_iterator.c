cb5747c5dbe908b528ce1c910ae5ca6f74dd3eed
zyclamenace <gemicieneshakki@gmail.com>
2024-07-16 13:03:36 +0200

    task 1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -2,19 +2,20 @@
 #include <stdlib.h>
 
 /**
- * array_iterator - 
+ * array_iterator - action for each array
  * @array: array
  * @action: pointeur de fonction
+ * @size: size of an array
  *
  * Return:
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    int i;
-    for (i = 0; array[i] < array[size]; i++)
-    {
-        action(array[i]);
-    }
-    
-    return;
+	int i;
+	for (i = 0; array[i] < array[size]; i++)
+	{
+		action(array[i]);
+	}
+
+	return;
 }

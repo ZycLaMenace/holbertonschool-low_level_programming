fe162280fb3b68b415b89fa4011a3acd87d1d529
zyclamenace <gemicieneshakki@gmail.com>
2024-07-18 14:15:27 +0200

    final task 3

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,30 +1,55 @@
 #include "3-calc.h"
 
-/*op_add - sum */
+/**
+ * op_add - sum
+ * 
+ * Return: a plus b
+ */
+
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
-/* op_sub - substract */
+/**
+ * op_sub - substract
+ * 
+ * Return: a minus b
+ */
+
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
-/* op_mul - multiply */
+/**
+ * op_mul - multiply
+ * 
+ * Return: a x b
+ */
+
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
-/* op_div - division */
+/**
+ * op_div - division
+ * 
+ * Return: divide a b
+*/
+
 int op_div(int a, int b)
 {
 	return (a / b);
 }
 
-/* op_mod - modulo (take the remainder of the division)*/
+/**
+ *  op_mod - modulo (take the remainder of the division)
+ * 
+ * Return: modulo a b
+*/
+
 int op_mod(int a, int b)
 {
 	return (a % b);

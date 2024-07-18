0c3036cc29b0043e301891c4054c798de887b8a7
zyclamenace <gemicieneshakki@gmail.com>
2024-07-18 14:04:59 +0200

    main c condition changes

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -2,10 +2,14 @@
 
 /**
  * main - do simple operations
- * 
-*/
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ *
+ * Return: 0 on success, 98 wrong number of arguments,
+ *         99 invalid operator, 100 division/modulo by zero
+ */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int num1, num2, result;
 	int (*operation)(int, int);
@@ -25,14 +29,13 @@ if (operation == NULL)
 	exit (99);
 }
 
- if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
 {
 	printf("Error\n");
 	exit (100);
 }
-    result = operation(num1, num2);
-    printf("%d\n", result);
+	result = operation(num1, num2);
+	printf("%d\n", result);
 
-    return (0);
-
+	return (0);
 }

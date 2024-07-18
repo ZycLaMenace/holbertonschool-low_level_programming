ecc45ab4a3cae9c04c975b7583d6b77f3854618d
zyclamenace <gemicieneshakki@gmail.com>
2024-07-18 14:02:07 +0200

    main c condition changes

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -10,15 +10,13 @@ int main (int argc, char *argv[])
 	int num1, num2, result;
 	int (*operation)(int, int);
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-
-
 if (argc != 4)
 {
 	printf("Error\n");
 	exit (98);
 }
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 	operation = get_op_func(argv[2]);
 
 if (operation == NULL)

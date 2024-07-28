af45358672a12b08682963eaecde315a519c1b91
zyclamenace <gemicieneshakki@gmail.com>
2024-07-29 01:39:05 +0200

    task 3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - multiply two int
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
+}

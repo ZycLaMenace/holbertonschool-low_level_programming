2836ae894e7e307198da768ab0d54d3e013cc514
zyclamenace <gemicieneshakki@gmail.com>
2024-07-29 19:00:09 +0200

    task 1 first try

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,1 +1,12 @@
 #include "lists.h"
+/**
+ * list_len - function that calcul of element
+ *@h: head
+ *
+ * Return: number of element
+ */
+
+size_t list_len(const list_t *h)
+{
+	printf("%d elements", &h);
+}

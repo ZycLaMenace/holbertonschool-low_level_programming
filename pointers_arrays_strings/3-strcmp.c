65147a3123767aa90c38e0233820618b328bc08e
zyclamenace <gemicieneshakki@gmail.com>
2024-07-29 16:39:46 +0200

    Grand nettoyage

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
-/**
- * _strcmp - comapre 2 string
+/*
+ * _strcmp - compare 2 string
  *
- * Return: Always 0 or -number or +number.
+ * Return: 0 or -number or +number.
  */
 
 int _strcmp(char *s1, char *s2)
 {
-		while (*s1 == *s2 && *s1)
+	while (*s1 == *s2 && *s1)
 	{
 		s1++;
 		s2++;
 	}
-
 	return (*s1 - *s2);
 }

9d5b62a5dcadf37a2ee64be5622c35d6342ea771
zyclamenace <gemicieneshakki@gmail.com>
2024-08-01 12:44:48 +0200

    fix problem

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -35,7 +35,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->prev = current;
 	new_node->next = current->next;
-
+	current->next->prev = new_node;
+	current->next = new_node;
 
 	return (new_node);
 }

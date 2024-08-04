df128527af2c39d033f0ba2c0077bb8a8e8e5843
zyclamenace <gemicieneshakki@gmail.com>
2024-08-04 23:42:49 +0200

    fixing task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -9,5 +9,39 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fd;
+	ssize_t nread, nwritten;
+	char *buffer;
 
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	nread = read(fd, buffer, letters);
+	if (nread == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	nwritten = write(STDOUT_FILENO, buffer, nread);
+
+	free(buffer);
+	close(fd);
+
+	if (nwritten == -1 || nwritten != nread)
+		return (0);
+
+	return (nwritten);
 }

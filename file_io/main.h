e379c1149d9dbcb6f087f5a5e82e56147f1c9cc1
zyclamenace <gemicieneshakki@gmail.com>
2024-08-04 23:45:01 +0200

    fix main.h

--- file_io/main.h
+++ file_io/main.h
@@ -9,4 +9,12 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
 
+#define BUFFER_SIZE 1024
+
+int close_file_free_buff(int file_descriptor, char *buf, int returnval);
+	/** Function to shorten close file, free buff and return value */
+
+int close_file_no_free(int file_descriptor, int returnval);
+	/** Function to shorten close file without freeing buff and return value */
+
 #endif /* MAIN_H */

e1695921022df816a9411f7b559c67520e84c497
zyclamenace <gemicieneshakki@gmail.com>
2024-08-04 18:52:33 +0200

    addind file_io dir and mandatory file (readme main.h)

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * read_textfile - Reads a text file and prints it to POSIX stdout
+ * @filename: Name of the file to read
+ * @letters: Number of letters to read and print
+ *
+ * Return: Actual number of letters read and printed, or 0 if it fails
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+}

d48d157be0f44ed26d38e594b91afd968534e00d
zyclamenace <gemicieneshakki@gmail.com>
2024-08-05 18:02:37 +0200

    fix

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -17,26 +17,28 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
-		return NULL;
+		return (NULL);
 
-	name_copy = strdup(name);
+	name_copy = malloc(strlen(name) + 1);
 	if (name_copy == NULL)
 	{
 		free(new_dog);
-		return NULL;
+		return (NULL);
 	}
+	strcpy(name_copy, name);
 
-	owner_copy = strdup(owner);
+	owner_copy = malloc(strlen(owner) + 1);
 	if (owner_copy == NULL)
 	{
 		free(name_copy);
 		free(new_dog);
-		return NULL;
+		return (NULL);
 	}
+	strcpy(owner_copy, owner);
 
 	new_dog->name = name_copy;
 	new_dog->age = age;
 	new_dog->owner = owner_copy;
 
-	return new_dog;
+	return (new_dog);
 }
